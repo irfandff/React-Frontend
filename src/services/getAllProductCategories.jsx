f8f4925ab012e589497473058e9ac6a536ec68f0
irfandff <irfandaffa1945@gmail.com>
2024-11-18 12:43:18 +0700

    first commit

---
+++ src/services/getAllProductCategories.jsx
@@ -0,0 +1,16 @@
+const getAllProductCategories = ()  => {
+    return [   {
+      id: 'HOODIE',
+      slug: 'hoodie',
+      name: `Hoodie`
+    },
+    {
+      id: 'T-SHIRT',
+      slug: 't-shirt',
+      name: `T-Shirt`
+    },
+    ]
+  }
+  
+  export default getAllProductCategories
+  
---
+++ src/services/getAllProducts.js
@@ -0,0 +1,93 @@
+export default function getAllProducts() {
+  return [
+    {
+      id: 'BH-O-HOODIE-001',
+      slug: 'black-hoodie-oversized-hoodie',
+      name: "Black Hoodie Oversized",
+      category: 'Hoodie',
+      categorySlug: 'hoodie',
+      price: 450_000,
+      stock: 100,
+      imageUrl: '/assets/images/Black Hoodie.jpg',
+      description: `A versatile black hoodie, suitable for casual or sporty looks, made from fleece material to keep you comfortable in any weather.`
+    },
+    {
+      id: 'GH-R-HOODIE-002',
+      slug: 'green-hoodie-reguler-hoodie',
+      name: "Green Hoodie Reguler",
+      category: 'Hoodie',
+      categorySlug: 'hoodie',
+      price: 320_000,
+      stock: 5,
+      imageUrl: `/assets/images/Green Hoodie.jpg`,
+      description: `A cozy green hoodie made of soft and warm fabric, complete with a front pocket and adjustable hood for maximum comfort.`
+    },
+    {
+      id: 'WT-R-T-SHIRT-003',
+      slug: 'white-tshirt-reguler-t-shirt',
+      name: "White T-Shirt Reguler",
+      category: 'T-Shirt',
+      categorySlug: 't-shirt',
+      price: 150_000,
+      stock: 0,
+      imageUrl: `/assets/images/White T-Shirt.jpg`,
+      description: `A premium cotton white t-shirt, comfortable for daily wear, perfect for casual or smart-casual styles.`
+    },
+    {
+      id: 'AT-R-T-SHIRT-004',
+      slug: 'painted-tshirt-oversized-t-shirt',
+      name: "Painted T-Shirt Oversized",
+      category: 'T-Shirt',
+      categorySlug: 't-shirt',
+      price: 200_000,
+      stock: 100,
+      imageUrl: '/assets/images/Art T-Shirt.jpg',
+      description: `A unique t-shirt featuring creative art designs, crafted from high-quality materials, perfect for those who love to stand out.`
+    },
+    {
+      id: 'BRT-R-T-SHIRT-005',
+      slug: 'brown-tshirt-reguler-t-shirt',
+      name: "Brown T-Shirt Reguler",
+      category: 'T-Shirt',
+      categorySlug: 't-shirt',
+      price: 150_000,
+      stock: 5,
+      imageUrl: `/assets/images/Brown T-Shirt.jpg`,
+      description: `A comfortable brown t-shirt crafted from premium cotton, ideal for a relaxed yet earthy and trendy vibe.`
+    },
+    {
+      id: 'SH-O-HOODIE-006',
+      slug: 'street-hoodie-oversized-hoodie',
+      name: "Street Hoodie Oversized",
+      category: 'Hoodie',
+      categorySlug: 'hoodie',
+      price: 450_000,
+      stock: 100,
+      imageUrl: '/assets/images/Street Hoodie.jpg',
+      description: `A modern streetwear-inspired hoodie with bold design elements, made for ultimate comfort and urban style.`
+    },
+    {
+      id: 'BLT-R-T-SHIRT-007',
+      slug: 'black-tshirt-oversized-t-shirt',
+      name: "Black T-Shirt Oversized",
+      category: 'T-Shirt',
+      categorySlug: 't-shirt',
+      price: 200_000,
+      stock: 100,
+      imageUrl: '/assets/images/Black T-Shirt.jpg',
+      description: `A classic black t-shirt with a minimalist design, made from lightweight and breathable fabric, ideal for any activity.`
+    },
+    {
+      id: 'WH-R-HOODIE-008',
+      slug: 'white-hoodie-reguler-hoodie',
+      name: "White Hoodie Reguler",
+      category: 'Hoodie',
+      categorySlug: 'hoodie',
+      price: 320_000,
+      stock: 0,
+      imageUrl: `/assets/images/White Hoodie.jpg`,
+      description: `A clean and stylish white hoodie, made from soft, warm fabric, perfect for layering or a minimalist casual look.`
+    },
+
+  ]
+}
